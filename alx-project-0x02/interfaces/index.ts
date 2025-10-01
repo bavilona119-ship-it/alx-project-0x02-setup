b1ac73cff73d69664b4bfb5af113a60c11d4c35d
bavilona119-ship-it <bavilona119@gmail.com>
2025-10-01 12:57:27 +0200

    Update index.ts

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -1,1 +1,13 @@
+// Example of a simple interface
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+// ✅ Interface pour la carte
+export interface CardProps {
+  title: string;
+  content: string;
+}
 
