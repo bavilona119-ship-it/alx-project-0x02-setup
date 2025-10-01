6c2a48b3f3ec2b1482509cb78ac2496384926efe
bavilona119-ship-it <bavilona119@gmail.com>
2025-10-01 12:53:20 +0200

    Update Header.tsx

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,1 +1,21 @@
+import React from "react";
+import Link from "next/link";
+
+const Header: React.FC = () => {
+  return (
+    <header className="bg-blue-600 text-white p-4 flex justify-center gap-6">
+      <Link href="/" className="hover:underline">
+        Home
+      </Link>
+      <Link href="/home" className="hover:underline">
+        Custom Home
+      </Link>
+      <Link href="/about" className="hover:underline">
+        About
+      </Link>
+    </header>
+  );
+};
+
+export default Header;
 
